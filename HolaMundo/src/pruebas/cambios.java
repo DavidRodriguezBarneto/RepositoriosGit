c79bbcac8429ca399643554561e0c783f6360736
DavidRodriguezBarneto <d.rodriguez.barneto@iescristobaldemonroy.es>
2023-03-16 09:53:58 +0100

    Segundo commit en la Rama

--- HolaMundo/src/pruebas/cambios.java
+++ HolaMundo/src/pruebas/cambios.java
@@ -5,6 +5,7 @@ public class cambios {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Cambio en la rama 1");
+		System.out.println("Segundo cambio");
 	}
 
 }
